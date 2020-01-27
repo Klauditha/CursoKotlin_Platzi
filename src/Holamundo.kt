c868a066230289573261bab348eb5a72d86204f4
Claudia Villarroel <claudinhavilla@hotmail.com>
2020-01-27 18:15:48 -0300

    ADD: Revision operadores con klotin

--- src/Holamundo.kt
+++ src/Holamundo.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
+    /*Clase  de Hola Mundo*/
     println("Hola mundo")
+
+    /*Clase  de Operadores en Kotlin*/
+    println(1+1)
+    println(3-1)
+    println(2*2)
+    println(4/2)
+    println(4%2)
+
+    val a = 4
+    val b = 2
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
 }
